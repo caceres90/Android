bf9a8728a9746e6808f33922aea760799d7ca8dd
Your Name <you@example.com>
2025-11-24 11:14:26 -0500

    Se agregaron los archivo PokemonRepositoryImpl y repositoryModule

--- app/src/main/java/com/sena/practica_de_consumo_api/data/repository/PokemonApi.kt
+++ app/src/main/java/com/sena/practica_de_consumo_api/data/repository/PokemonApi.kt
@@ -6,5 +6,5 @@ import retrofit2.http.GET
 
 interface PokemonApi {
     @GET(value = "pokemon")
-    suspend fun fetchPokemons(): PokemonListResponseDto
+    suspend fun getPokemons(): PokemonListResponseDto
 }
--- app/src/main/java/com/sena/practica_de_consumo_api/di/RepositoryModule.kt
+++ app/src/main/java/com/sena/practica_de_consumo_api/di/RepositoryModule.kt
@@ -1,4 +1,22 @@
 package com.sena.practica_de_consumo_api.di
 
+import com.sena.practica_de_consumo_api.data.repository.PokemonRepositoryImpl
+import com.sena.practica_de_consumo_api.domain.repository.PokemonRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(
+        pokemonRepository: PokemonRepositoryImpl
+    ): PokemonRepository {
+        return pokemonRepository
+    }
 }
